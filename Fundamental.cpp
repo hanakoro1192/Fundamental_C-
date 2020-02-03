db4f065949d8c5254ed17e46fadb3d32de7c7d9b
宇都宮靖之 <utsunomiyayasuyuki@utsunomiyayasuyukinoMacBook-Pro.local>
2020-02-03 06:57:48 -0800

    commit

--- Fundamental.cpp
+++ Fundamental.cpp
@@ -474,3 +474,35 @@ int main(){
         cout << "答えは" << result << " です。" << endl;
     }
 }
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i = 0; i < 5; ++i){
+        int a, b, op, result;
+
+        cout << "第一項" << flush;
+        cin >> a;
+
+        cout << "第二項" << flush;
+        cin >> b;
+
+        cout << "1:可算, 2:減算, 3:乗算, 4:除算 > " << flush;
+        cin >> op;
+
+        if(op == 1){
+            result = a + b;
+        }else if(op == 2){
+            result = a - b;
+        }else if(op == 3){
+            result = a * b;
+        }else if(op == 4){
+            result = a / b;
+        }else{
+            cout << "真面目にせんかい！！" << endl;
+            continue;
+        }
+        cout << "答えは" << result << " です。" << endl;
+    }
+}
