e888749976e0aab28c86803abd0353f946a32eae
宇都宮靖之 <utsunomiyayasuyuki@utsunomiyayasuyukinoMacBook-Pro.local>
2020-02-19 00:11:03 -0800

    20200219

---
+++ Fundamental2.cpp
@@ -0,0 +1,16 @@
+//要点
+//アドレスに*をつけるとそのアドレスにあるメモリを扱える//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a; 
+    int* p = &a;
+
+    a = 0;
+    cout << " a = " << a << endl << " *p = " << *p << endl;
+
+    *p = 5;
+    cout << " a = " << a << endl << " *p = " << *p << endl;
+}
