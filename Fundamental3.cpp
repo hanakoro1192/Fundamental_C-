d236eff3b56522018297d4c250092cd9fe0ef653
宇都宮靖之 <csya18003@g.nihon-u.ac.jp>
2020-03-14 22:41:32 -0700

    20200315

--- Fundamental3.cpp
+++ Fundamental3.cpp
@@ -169,3 +169,52 @@ class IntArray{
     int* m_array; //動的配列
     int m_size; //配列の要素数
 }
+
+//インフライン定義:ここでは Color クラスの宣言の中に Brightness() メンバ関数の定義も書きました。このような定義の仕方をインライン定義という//
+class Color{
+    public:
+        UInt08 r; // R 要素
+        UInt08 g; //G 要素
+        UInt08 b; //B 要素
+
+        UInt08 Brightness();
+};
+
+UInt08 Color::Brightness(){
+    return MAX(r, MAX(g,b));
+}
+
+
+/**
+ * メンバ変数の初期化はコンストラクタで行う
+ * コンストラクタはクラス名と同じ名前の、戻り値なしの関数となる
+ * コンストラクタはオブジェクトを作るときに自動的に呼ばれる */
+
+#include <iostream>
+using namespace std;
+
+class Integer{
+    public:
+        int m_value;
+
+        Integer(); //ここで持ってくる
+        void show(); //返り値あり
+};
+
+//コンストラクタ
+Integer::Integer(){
+    m_value = 0;
+}
+
+//メンバ変数の表示
+void Integer::show(){
+    cout << m_value << endl;
+}
+
+int main(){
+    Integer a, b, c;
+
+    a.show();
+    b.show();
+    c.show();
+}
